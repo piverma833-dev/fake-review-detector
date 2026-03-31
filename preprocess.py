265e8f51d55ff171761a1f4a6d761981ddc3b4ef
piverma833-dev <piverma833@gmail.com>
2026-03-31 22:56:00 +0530

    Update preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -1,25 +1,14 @@
 import string
 from nltk.corpus import stopwords
-from nltk.tokenize import word_tokenize
 from nltk.stem import WordNetLemmatizer
-import nltk
-
-resources = ["punkt", "stopwords", "wordnet", "omw-1.4"]
-for res in resources:
-    try:
-        nltk.data.find(f"tokenizers/{res}" if res == "punkt" else f"corpora/{res}")
-    except LookupError:
-        nltk.download(res, quiet=True)
 
 def processing_text(text):
     text = text.lower()
-    clean_text = ""
+    clean_text = "".join([ch for ch in text if ch not in string.punctuation])
 
-    for ch in text:
-        if ch not in string.punctuation:
-            clean_text += ch
 
-    tokens = word_tokenize(clean_text)
+    tokens = clean_text.split()
+    
     stop_words = set(stopwords.words('english'))
     lemmatizer = WordNetLemmatizer()
 
